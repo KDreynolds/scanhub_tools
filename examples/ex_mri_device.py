8cb956ad88167d8e7fcc2519aceba75e42f44d34
Christoph Dinh <christoph.dinh@mne-cpp.org>
2022-04-29 21:56:40 +0200

    add example mri device

---
+++ examples/ex_mri_device.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# @authors   Christoph Dinh <christoph.dinh@brain-link.de>
+# @version   1.0
+# @date      April, 2022
+# @copyright Copyright (c) 2022, BRAIN-LINK UG and authors of ScanHub Tools. All rights reserved.
+# @license   BSD 3-Clause License
+# @brief     Example MRI device
+# ---------------------------------------------------------------------------
+
+#%% Imports
+import scanhub
+
+#%% 
